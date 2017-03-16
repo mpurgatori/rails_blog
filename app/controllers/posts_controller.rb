49f93299b77ad47b6c7636db299800ae74c2217d
Mike Purgatori <mpurgatori@gmail.com>
2017-03-15 21:06:10 -0500

    Created post routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,30 @@
 class PostsController < ApplicationController
   def index
+    @content_first = "This is the first post"
+    @content_second = "And this is the second post"
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
 
   end
 end
